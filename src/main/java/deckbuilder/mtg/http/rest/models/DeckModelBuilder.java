6d46493e649cc78468c1e9e4385a62a7079660d1
Jared Pearson <jaredapearson@gmail.com>
2014-06-22 06:09:47 -0700

    Made the DeckModelBuilder mutable.

--- src/main/java/deckbuilder/mtg/http/rest/models/DeckModelBuilder.java
+++ src/main/java/deckbuilder/mtg/http/rest/models/DeckModelBuilder.java
@@ -5,6 +5,7 @@ import javax.annotation.Nonnull;
 import deckbuilder.mtg.entities.Deck;
 import deckbuilder.mtg.entities.User;
 import deckbuilder.mtg.http.rest.Builder;
+import deckbuilder.mtg.http.rest.UrlBuilder;
 import deckbuilder.mtg.http.rest.DeckIdCardsResource;
 import deckbuilder.mtg.http.rest.EntityUrlFactory;
 
@@ -13,22 +14,70 @@ import deckbuilder.mtg.http.rest.EntityUrlFactory;
  * @author jared.pearson
  */
 public class DeckModelBuilder implements Builder<DeckModel> {
-	private final EntityUrlFactory urlFactory;
-	private final Deck deck;
+	private UrlBuilder url;
+	private Long id;
+	private String name;
+	private UrlBuilder cardsUrl;
+	private UrlBuilder ownerUrl;
+	
+	public DeckModelBuilder() {
+	}
 	
 	public DeckModelBuilder(@Nonnull EntityUrlFactory urlFactory, @Nonnull Deck deck) {
 		assert urlFactory != null;
 		assert deck != null;
-		this.urlFactory = urlFactory;
-		this.deck = deck;
+
+		this.url = urlFactory.createEntityUrl(Deck.class, deck.getId());
+		this.id = deck.getId();
+		this.name = deck.getName();
+		this.cardsUrl = new DeckIdCardsResource.UrlBuilder(deck.getId());
+		this.ownerUrl = urlFactory.createEntityUrl(User.class, deck.getOwner().getId());
 	}
 	
+	public UrlBuilder getUrl() {
+		return url;
+	}
+	
+	public void setUrl(UrlBuilder urlBuilder) {
+		this.url = urlBuilder;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public UrlBuilder getCardsUrl() {
+		return cardsUrl;
+	}
+
+	public void setCardsUrl(UrlBuilder cardsUrl) {
+		this.cardsUrl = cardsUrl;
+	}
+	
+	public UrlBuilder getOwnerUrl() {
+		return ownerUrl;
+	}
+
+	public void setOwnerUrl(UrlBuilder ownerUrl) {
+		this.ownerUrl = ownerUrl;
+	}
+
 	public DeckModel build() {
-		final String url = urlFactory.createEntityUrl(Deck.class, deck.getId()).build();
-		final long id = deck.getId();
-		final String name = deck.getName();
-		final String cardsUrl = new DeckIdCardsResource.UrlBuilder(deck.getId()).build();
-		final String ownerUrl = urlFactory.createEntityUrl(User.class, deck.getOwner().getId()).build();
+		String url = this.url.build();
+		String cardsUrl = this.cardsUrl.build();
+		String ownerUrl = this.ownerUrl.build();
 		return new DeckModel(url, id, name, cardsUrl, ownerUrl);
 	}
 }
