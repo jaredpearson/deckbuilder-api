da30476a526126d03916c4993c20165fce65d9e3
Jared Pearson <jaredapearson@gmail.com>
2014-06-28 22:21:48 -0700

    Simplified the Main code

--- src/main/java/deckbuilder/mtg/Main.java
+++ src/main/java/deckbuilder/mtg/Main.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.sql.DataSource;
 
@@ -40,18 +41,14 @@ import deckbuilder.mtg.service.impl.DefaultDeckService;
 import deckbuilder.mtg.service.impl.DefaultUserService;
 
 public class Main {
-	private static final Command COMMAND_RUN = new Command("run", "Starts the server");
-	private static final Command COMMAND_DBINIT = new Command("dbinit", "Initializes the database by executing the DDL");
-	private static final Command COMMAND_DBIMPORTSETS = new Command("dbimportsets", "Imports the set data from the files specified as arguments.");
-	private static final Command[] COMMANDS;
-	
-	static {
-		COMMANDS = new Command[]{
-			COMMAND_RUN,
-			COMMAND_DBINIT,
-			COMMAND_DBIMPORTSETS
-		};
-	}
+	private static final Command COMMAND_RUN = new Command("run", "Starts the server", new RunCommandRunnable());
+	private static final Command COMMAND_DBINIT = new Command("dbinit", "Initializes the database by executing the DDL", new DbInitCommandRunnable());
+	private static final Command COMMAND_DBIMPORTSETS = new Command("dbimportsets", "Imports the set data from the files specified as arguments.", new DbImportSetsCommandRunnable());
+	private static final Command[] COMMANDS = new Command[]{
+		COMMAND_RUN,
+		COMMAND_DBINIT,
+		COMMAND_DBIMPORTSETS
+	};
 	
 	private Main() {
 	}
@@ -62,36 +59,15 @@ public class Main {
 			return;
 		}
 		
-		Command command = getCommand(args[0]);
+		final Command command = getCommand(args[0]);
 		if(command == null) {
 			System.out.println("Unknown command specified: " + args[0]);
 			printUsage(System.out);
 			return;
 		}
 		
-		String[] commandArgs = (args.length == 1) ? new String[0] : Arrays.copyOfRange(args, 1, args.length);
-		
-		if(COMMAND_RUN.equals(command)) {
-			HttpServer httpServer = createAppInjector().getInstance(HttpServer.class);
-			httpServer.startServer();
-			
-		} else if(COMMAND_DBINIT.equals(command)) {
-			DatabaseService dbService = createAppInjector().getInstance(DatabaseService.class);
-			dbService.initializeSchema();
-			
-		} else if(COMMAND_DBIMPORTSETS.equals(command)) {
-			if(commandArgs.length == 0) {
-				System.out.println("No files passed as arguments.");
-				printUsage(System.out);
-				return;
-			}
-			String[] setPaths = commandArgs;
-			DatabaseService dbService = createAppInjector().getInstance(DatabaseService.class);
-			dbService.importSets(setPaths);
-			
-		} else {
-			throw new IllegalStateException();
-		}
+		final String[] commandArgs = (args.length == 1) ? new String[0] : Arrays.copyOfRange(args, 1, args.length);
+		command.run(commandArgs);
 	}
 
 	/**
@@ -130,141 +106,153 @@ public class Main {
 		return null;
 	}
 	
-	private static Injector createAppInjector() {
-		//the database url is given in following format
-		// [type]://[username]:[password]@[host]:[port]
-		String databaseUrl = System.getenv("DATABASE_URL");
-		if(databaseUrl == null) {
-			throw new RuntimeException("DATABASE_URL environment variable not found.");
-		}
-		final DatabaseCredentials dbCreds = (new DatabaseUrlParser()).parseDatabaseUrl(databaseUrl);
-		
-		//setup the persitance module
-		JpaPersistModule persistModule = new JpaPersistModule("deckbuilder.mtg");
-		Properties properties = new Properties();
-		properties.put("hibernate.dialect", dbCreds.getHibernateDialect());
-	    properties.put("javax.persistence.jdbc.driver", dbCreds.getJdbcDriverClassName());
-	    properties.put("javax.persistence.jdbc.url", dbCreds.getJdbcUrl());
-	    properties.put("javax.persistence.jdbc.user", dbCreds.getUsername());
-	    properties.put("javax.persistence.jdbc.password", dbCreds.getPassword());
-		persistModule.properties(properties);
-		
-		return Guice.createInjector(persistModule, 
-				
-			//Database/Datasource module
-			new AbstractModule() {
-				@Override
-				protected void configure() {
-					bind(DatabaseCredentials.class).toInstance(dbCreds);
-					bind(DataSource.class).toProvider(DataSourceProvider.class).in(Singleton.class);
-					bind(DatabaseService.class).in(Singleton.class);
-				}
-			}, 
-			
-			//Application module
-			new AbstractModule() {
-				@Override
-				protected void configure() {
-					//Application specific services module
-					bind(UserService.class).to(DefaultUserService.class).in(Singleton.class);
-					bind(CardService.class).to(DefaultCardService.class).in(Singleton.class);
-					bind(DeckService.class).to(DefaultDeckService.class).in(Singleton.class);
-					bind(DeckCardService.class).to(DefaultDeckCardService.class).in(Singleton.class);
-					bind(CardSetService.class).to(DefaultCardSetService.class).in(Singleton.class);
-				}
-				
-				@Provides
-				@Named("currentVersion")
-				@Singleton
-				private Version currentVersion() {
-					return new Version("v1");
-				}
-				
-				@Provides
-				@Singleton
-				private Configuration loadConfig() {
-					//load the properties from the standard location
-					Properties properties = new Properties();
-					try(InputStream inputStream = getClass().getResourceAsStream("/deckbuilder/mtg/config.properties")) {
-						properties.load(inputStream);
-					} catch(IOException exc) {
-						throw new RuntimeException(exc);
-					}
-					
-
-					//get the administrators
-					final List<String> administratorNames;
-					final String adminNamesAsString = properties.getProperty("administrators");
-					if(adminNamesAsString != null) {
-						administratorNames = Arrays.asList(adminNamesAsString.split(","));
-					} else {
-						administratorNames = Collections.emptyList();
-					}
-					
-					// get the cors.* properties
-					final String corsAllowedOrigins = properties.getProperty("cors.allowedOrigins", ""); 
-					CorsConfiguration corsConfig = new CorsConfiguration(corsAllowedOrigins);
-					
-					return new Configuration(administratorNames, corsConfig);
-				}
-			},
-			
-			//HTTP Server module
-			new AbstractModule() {
-				@Override
-				protected void configure() {
-					bind(HttpServer.class).in(Singleton.class);
-				}
-				
-				@Provides 
-				@Singleton
-				@Named("HTTP Port")
-				public int getHttpPort() {
-					
-					//default to port 8080
-					int port = 8080;
-					
-					//attempt to get the port from the environment variable
-					String portString = System.getenv("PORT");
-					if(portString != null) {
-						port = Integer.valueOf(portString);
-					}
-					
-					return port;
-				}
-			});
-	}
-	
 	/**
-	 * Creates instances of {@link DataSource} using the {@link DatabaseCredentials}
-	 * within the container.
+	 * Factory for creating instances of {@link Injector}
 	 * @author jared.pearson
 	 */
-	private static class DataSourceProvider implements Provider<DataSource> {
-		@Inject
-		DatabaseCredentials credentials;
+	public static class InjectorFactory {
 		
-		@Override
-		public DataSource get() {
-			try {
-				Properties dbProps = new Properties();
-				dbProps.setProperty("url", credentials.getJdbcUrl());
-				dbProps.setProperty("user", credentials.getUsername());
-				dbProps.setProperty("password", credentials.getPassword());
-				return JDBCDataSourceFactory.createDataSource(dbProps);
-			} catch(Exception exc) {
-				throw new RuntimeException(exc);
+		/**
+		 * Creates the injector instance used for starting the application.
+		 */
+		public @Nonnull Injector createAppInjector() {
+			//the database url is given in following format
+			// [type]://[username]:[password]@[host]:[port]
+			String databaseUrl = System.getenv("DATABASE_URL");
+			if(databaseUrl == null) {
+				throw new RuntimeException("DATABASE_URL environment variable not found.");
+			}
+			final DatabaseCredentials dbCreds = (new DatabaseUrlParser()).parseDatabaseUrl(databaseUrl);
+			
+			//setup the persitance module
+			JpaPersistModule persistModule = new JpaPersistModule("deckbuilder.mtg");
+			Properties properties = new Properties();
+			properties.put("hibernate.dialect", dbCreds.getHibernateDialect());
+		    properties.put("javax.persistence.jdbc.driver", dbCreds.getJdbcDriverClassName());
+		    properties.put("javax.persistence.jdbc.url", dbCreds.getJdbcUrl());
+		    properties.put("javax.persistence.jdbc.user", dbCreds.getUsername());
+		    properties.put("javax.persistence.jdbc.password", dbCreds.getPassword());
+			persistModule.properties(properties);
+			
+			return Guice.createInjector(persistModule, 
+					
+				//Database/Datasource module
+				new AbstractModule() {
+					@Override
+					protected void configure() {
+						bind(DatabaseCredentials.class).toInstance(dbCreds);
+						bind(DataSource.class).toProvider(DataSourceProvider.class).in(Singleton.class);
+						bind(DatabaseService.class).in(Singleton.class);
+					}
+				}, 
+				
+				//Application module
+				new AbstractModule() {
+					@Override
+					protected void configure() {
+						//Application specific services module
+						bind(UserService.class).to(DefaultUserService.class).in(Singleton.class);
+						bind(CardService.class).to(DefaultCardService.class).in(Singleton.class);
+						bind(DeckService.class).to(DefaultDeckService.class).in(Singleton.class);
+						bind(DeckCardService.class).to(DefaultDeckCardService.class).in(Singleton.class);
+						bind(CardSetService.class).to(DefaultCardSetService.class).in(Singleton.class);
+					}
+					
+					@Provides
+					@Named("currentVersion")
+					@Singleton
+					private Version currentVersion() {
+						return new Version("v1");
+					}
+					
+					@Provides
+					@Singleton
+					private Configuration loadConfig() {
+						//load the properties from the standard location
+						Properties properties = new Properties();
+						try(InputStream inputStream = getClass().getResourceAsStream("/deckbuilder/mtg/config.properties")) {
+							properties.load(inputStream);
+						} catch(IOException exc) {
+							throw new RuntimeException(exc);
+						}
+						
+	
+						//get the administrators
+						final List<String> administratorNames;
+						final String adminNamesAsString = properties.getProperty("administrators");
+						if(adminNamesAsString != null) {
+							administratorNames = Arrays.asList(adminNamesAsString.split(","));
+						} else {
+							administratorNames = Collections.emptyList();
+						}
+						
+						// get the cors.* properties
+						final String corsAllowedOrigins = properties.getProperty("cors.allowedOrigins", ""); 
+						CorsConfiguration corsConfig = new CorsConfiguration(corsAllowedOrigins);
+						
+						return new Configuration(administratorNames, corsConfig);
+					}
+				},
+				
+				//HTTP Server module
+				new AbstractModule() {
+					@Override
+					protected void configure() {
+						bind(HttpServer.class).in(Singleton.class);
+					}
+					
+					@Provides 
+					@Singleton
+					@Named("HTTP Port")
+					public int getHttpPort() {
+						
+						//default to port 8080
+						int port = 8080;
+						
+						//attempt to get the port from the environment variable
+						String portString = System.getenv("PORT");
+						if(portString != null) {
+							port = Integer.valueOf(portString);
+						}
+						
+						return port;
+					}
+				});
+		}
+		
+		/**
+		 * Creates instances of {@link DataSource} using the {@link DatabaseCredentials}
+		 * within the container.
+		 * @author jared.pearson
+		 */
+		private static class DataSourceProvider implements Provider<DataSource> {
+			@Inject
+			DatabaseCredentials credentials;
+			
+			@Override
+			public DataSource get() {
+				try {
+					Properties dbProps = new Properties();
+					dbProps.setProperty("url", credentials.getJdbcUrl());
+					dbProps.setProperty("user", credentials.getUsername());
+					dbProps.setProperty("password", credentials.getPassword());
+					return JDBCDataSourceFactory.createDataSource(dbProps);
+				} catch(Exception exc) {
+					throw new RuntimeException(exc);
+				}
 			}
 		}
 	}
 	
 	private static class Command {
-		private String command;
-		private String helpText;
+		private final String command;
+		private final String helpText;
+		private final CommandRunnable runnable;
 		
-		public Command(String command, String helpText) {
+		public Command(String command, String helpText, CommandRunnable runnable) {
 			this.command = command;
 			this.helpText = helpText;
+			this.runnable = runnable;
 		}
 		
 		public String getCommand() {
@@ -273,6 +261,54 @@ public class Main {
 		
 		public String getHelpText() {
 			return helpText;
+		}
+		
+		/**
+		 * Executes the command
+		 * @param args arguments for the command
+		 */
+		public void run(String[] args) throws Exception {
+			runnable.run(args);
+		}
+		
+		/**
+		 * Closure for the execution of the command
+		 * @author jared.pearson
+		 */
+		public interface CommandRunnable {
+			public void run(String[] args) throws Exception;
+		}
+	}
+	
+	private static class RunCommandRunnable implements Command.CommandRunnable {
+		@Override
+		public void run(String[] args) throws Exception {
+			final InjectorFactory injectorFactory = new InjectorFactory();
+			final HttpServer httpServer = injectorFactory.createAppInjector().getInstance(HttpServer.class);
+			httpServer.startServer();
+		}
+	}
+	
+	private static class DbInitCommandRunnable implements Command.CommandRunnable {
+		@Override
+		public void run(String[] args) throws Exception {
+			final InjectorFactory injectorFactory = new InjectorFactory();
+			final DatabaseService dbService = injectorFactory.createAppInjector().getInstance(DatabaseService.class);
+			dbService.initializeSchema();
+		}
+	}
+	
+	private static class DbImportSetsCommandRunnable implements Command.CommandRunnable {
+		@Override
+		public void run(String[] args) throws Exception {
+			if(args.length == 0) {
+				System.out.println("No files passed as arguments.");
+				printUsage(System.out);
+				return;
+			}
+			final InjectorFactory injectorFactory = new InjectorFactory();
+			final DatabaseService dbService = injectorFactory.createAppInjector().getInstance(DatabaseService.class);
+			dbService.importSets(args);
 		}
 	}
 }
